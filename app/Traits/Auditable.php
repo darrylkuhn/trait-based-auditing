11cb28db923d72b025efb2872e2eea13d14aadd7
Darryl Kuhn <darryl@aristamd.com>
2016-04-25 21:56:45 +0000

    adds auditing support for additional eloquent events (read, update, delete)

--- app/Traits/Auditable.php
+++ app/Traits/Auditable.php
@@ -6,11 +6,40 @@ trait Auditable
 {
     public static function bootAuditable()
     {
+        self::loading( function ($obj)
+        {
+            $obj->auditRead();
+            return true;
+        } );
+
         self::created( function ($obj)
         {
             $obj->auditCreate();
             return true;
         } );
+
+        self::updated( function ($obj)
+        {
+            $obj->auditUpdate();
+            return true;
+        } );
+
+        self::deleted( function ($obj)
+        {
+            $obj->auditDelete();
+            return true;
+        } );
+    }
+
+    /**
+     * Writes to the log whenever this object is filled from the database
+     *
+     * @return void
+     */
+    public function auditRead()
+    {
+        $className = get_class( $this );
+        Log::info( "$className({$this->id}) was read", ['event' => 'read', 'entity' => "$className({$this->id})"] );
     }
 
     /**
@@ -22,6 +51,60 @@ trait Auditable
     {
         $className = get_class( $this );
         Log::info( "$className({$this->id}) was created", ['event' => 'create', 'entity' => "$className({$this->id})"] );
+    }
+
+    /**
+     * Writes to the log whenever this object is updated. Include a list of items
+     *
+     * @return void
+     */
+    public function auditUpdate()
+    {
+        $className = get_class( $this );
+        Log::info( "$className({$this->id}) was updated", ['event' => 'update', 'entity' => "$className({$this->id})"] );
+    }
+
+    /**
+     * Writes to the log whenever this object is deleted (soft delete or hard)
+     *
+     * @return void
+     */
+    public function auditDelete()
+    {
+        $className = get_class( $this );
+        Log::info( "$className({$this->id}) was deleted", ['event' => 'delete', 'entity' => "$className({$this->id})"] );
+    }
+
+    /**
+     * Register a loading model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public static function loading( $callback )
+    {
+        static::registerModelEvent( 'loading', $callback );
+    }
+
+    public function getObservableEvents()
+    {
+        return array_merge( parent::getObservableEvents(), ['loading'] );
+    }
+
+    /**
+     * Create a new model instance that is existing.
+     *
+     * @param  array  $attributes
+     * @return \Illuminate\Database\Eloquent\Model|static
+     */
+    public function newFromBuilder( $attributes=array(), $connection=null )
+    {
+        $instance = parent::newFromBuilder( $attributes, $connection );
+
+        // fire a loading event so we can fire actions as appropriate
+        $instance->fireModelEvent( 'loading' );
+
+        return $instance;
     }
 
 }
